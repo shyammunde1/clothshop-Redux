759d425f649c5bcdcab4f42a647a3388ec26043d
shyammunde1 <shyammunde1@gmail.com>
2023-08-21 11:23:02 +0530

    This is a clothshop-redux repo and create the redux store and deploy

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,52 @@
+import { Fragment, useContext } from 'react';
+import { Outlet } from 'react-router-dom';
+
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
+// import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
+
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
+import {
+  NavigationContainer,
+  NavLinks,
+  NavLink,
+  LogoContainer,
+} from './navigation.styles';
+import { useSelector } from 'react-redux';
+import { selectCurrentUser } from '../../store/user/user.selector';
+
+const Navigation = () => {
+  const currentUser = useSelector(selectCurrentUser)
+  const { isCartOpen } = useContext(CartContext);
+  console.log(currentUser)
+
+  return (
+    <Fragment>
+      <NavigationContainer>
+        <LogoContainer to='/'>
+          <CrwnLogo className='logo' />
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to='/shop'>SHOP</NavLink>
+
+          {currentUser ? (
+            <NavLink as='span' onClick={signOutUser}>
+              SIGN OUT
+            </NavLink>
+          ) : (
+            <NavLink to='/auth'>SIGN IN</NavLink>
+          )}
+          <CartIcon />
+        </NavLinks>
+        {isCartOpen && <CartDropdown />}
+      </NavigationContainer>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import  {compose,legacy_createStore,applyMiddleware} from 'redux'
+import logger from 'redux-logger'
+import { rootReducer } from './root-reducer'
+
+
+const middlewares =[logger]
+
+const composedEnhancers = compose(applyMiddleware(...middlewares))
+
+export const store= legacy_createStore(rootReducer,undefined,composedEnhancers)
+
